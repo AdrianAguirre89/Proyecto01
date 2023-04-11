3272cfb9b4cf4fdccfda6c0a30123f63d9f22e85
AdrianAguirre89 <130084086+AdrianAguirre89@users.noreply.github.com>
2023-04-11 16:16:41 -0300

    primer tarea

---
+++ sprint-1.js
@@ -0,0 +1,39 @@
+class productManager{
+    constructor(){
+        this.Products = []
+       
+    }
+    getProduct(){
+        console.log(this.Products)
+        return this.Products
+    }
+
+    addProduct({title,description,price,thumbnail,stock,}) {
+        let id = 1
+        if (this.Products.length===0){
+            id = 1
+        } else {
+            let lastProducts = this.Products[this.Products.length-1]
+            id = lastProducts.id + 1
+        }
+        let products = { title,description,price,thumbnail,stock,id }
+        this.Products.push(products)
+    }
+    getProductById(id){
+        let code = this.Products.find(elemento =>{
+            return elemento.id===id
+        })
+        if(this.id){
+            console.log(code)
+        }else{
+          console.log("Not found")  
+        };
+        
+    }
+
+}
+
+let ticket = new productManager
+ticket.addProduct({title:"producto prueba",description:"este es un producto prueba",price:200,thumbnail:"sin imagen",stock:25,})
+ticket.getProduct()
+ticket.getProductById()
